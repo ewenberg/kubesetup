97d8cb9cfca1f5b77cc68943a1dc64ab2d2d9e5c
ewenberg <ewenberg@gmail.com>
2024-05-10 18:19:06 -0400

    update host key script

--- generate_host_keys.sh
+++ generate_host_keys.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# generate keypair for the jumpbox
 ssh-keygen -t rsa -N "" -f id_rsa
 mkdir -p ~/.ssh
 mv id_rsa ~/.ssh
 mv id_rsa.pub ~/.ssh
 
+# copy jumpbox public key to other hosts
+while read IP FQDN HOST SUBNET; do 
+  ssh-copy-id root@${IP}
+done < machines.txt
 
+# vertify public key access is working
+while read IP FQDN HOST SUBNET; do 
+  ssh -n root@${IP} uname -o -m
+done < machines.txt
